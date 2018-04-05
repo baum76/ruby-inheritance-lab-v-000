3911c6028c4e93923c39b0fbae6e20ca2fe12d90
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-04-05 16:06:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,14 +1,13 @@
 require_relative "./user.rb"
 
 class Student < User
-  attr_accessor :knowledge
 
-  def initialize(knowledge)
+  def initialize
     @knowledge = []
   end
 
-  def learn("Ruby framework Rails gem bundle update")
-    @knowledge << "Ruby framework Rails gem bundle update"
+  def learn(string)
+    @knowledge << string
   end
 
   def knowledge
